1ea7036b6e47e86a9ff1bfea7b9580f4d076bd05
Tobitx-droid <tobitamofaadubredu@gmail.com>
2022-06-08 09:24:04 -0700

    C practice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Point of entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s[30] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; s[i] != ('\0'); i++)
+		putchar(s[i]);
+	putchar('\n');
+
+	return (0);
+}
