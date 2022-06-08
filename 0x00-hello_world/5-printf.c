f56e0095df8e53047890f51f3636e211d4cd09e7
Tobitx-droid <tobitamofaadubredu@gmail.com>
2022-06-07 19:17:26 -0700

    C programming practice

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-short description
+ * printf a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
