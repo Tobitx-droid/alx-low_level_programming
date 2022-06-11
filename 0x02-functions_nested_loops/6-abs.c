a2dbab80b6dc7c5d6eac59b623bb83a0a27ce428
Tobitx-droid <tobitamofaadubredu@gmail.com>
2022-06-10 17:32:42 -0700

    computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - Major program
- * finds absolute value of numbers
- * return 0 for success
+ * _abs - The description is below
+ * Description: Finds absolute value of numbers
+ * @i: integer input value
+ * Return: Always 0 for success
  */
 
 int _abs(int i)
