db10039e775b1594cb882eaa12d158d9d1b1266a
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-13 04:19:57 +0000

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - The description is below
+ * Description: Multiplies two integers and returns the output
+ * @a: First integer
+ * @b: Second integer
+ * Return: returns the multiple of two numbers
+ */
+
+int mul(int a, int b)
+{
+	int final;
+
+	final = a * b;
+
+	return (final);
+}
