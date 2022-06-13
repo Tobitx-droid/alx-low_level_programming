46bc0596b708f8bcee91a0728457d8e52781a7ff
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-13 16:30:04 +0000

    Create print_triangle.c

---
+++ 0x04-more_functions_nested_loops/print_triangle.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - The description is below
+ * Description - prints a triangle, followed by a new line
+ * @size: size of the triangle
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int k, m;
+
+		for (k = 1; k <= size; k++)
+		{
+			for (m = k; m < size; m++)
+			{
+				_putchar(' ');
+			}
+
+			for (m = 1; m <= k; m++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
