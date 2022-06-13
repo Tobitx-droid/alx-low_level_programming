5777002456c4c5baafaf20b3686d84bc434147a6
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-13 16:23:32 +0000

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,23 +2,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry piont
- * Return: Always 0
+ * main - The description is below
+ * Description: This code is for and interview purpose
+ * Return: Always 0 for success
  */
 int main(void)
 {
-	      int i = 1;
+	      int j = 1;
 
-	      for (; i < 100 ; i++)
+	      for (; j < 100 ; j++)
 	      {
-	            	if (i % 3 == 0 && i % 5 == 0)
+	            	if (j % 3 == 0 && j % 5 == 0)
 		                  	printf("FizzBuzz ");
-	            	else if (i % 3 == 0)
+	            	else if (j % 3 == 0)
 	                  		printf("Fizz ");
-	            	else if (i % 5 == 0)
+	            	else if (j % 5 == 0)
 	                  		printf("Buzz ");
 	             	else
-	                  		printf("%d ", i);
+	                  		printf("%d ", j);
       	}
       	printf("Buzz\n");
 
