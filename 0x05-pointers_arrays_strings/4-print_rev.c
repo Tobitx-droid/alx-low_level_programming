942cf8b645783dc77929c9b4014de2875b61f3b1
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-14 04:32:39 +0000

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - The description is below
+ * Description: Prints a string in reverse
+ * @s: String input
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	      int i;
+	      int total;
+
+	      for (total = 0; s[total] != '\0'; total++);
+
+	      i = total - 1;
+	      while (i >= 0)
+	      {
+	            	_putchar(s[i]);
+	            	i--;
+      	}
+      	_putchar('\n');
+}
