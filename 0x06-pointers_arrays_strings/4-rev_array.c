e0027f422a62b847e478d798d8f6cc6cce0ccc54
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-17 01:56:01 +0000

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - The description is below
+ * Description: reverses an array of integers
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ * Retu
+ */
+void reverse_array(int *a, int n)
+{
+	int k, g, tmp;
+
+	g = n - 1;
+
+	for (k = 0; k < n / 2; k++)
+	{
+		tmp = a[k];
+		a[k] = a[g];
+		a[g--] = tmp;
+	}
+}
