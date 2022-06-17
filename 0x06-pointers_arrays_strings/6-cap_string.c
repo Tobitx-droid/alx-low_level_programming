7e9b2a804dfb374b6b8d559808dc67f582cac2cc
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-17 02:01:34 +0000

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+	#include "main.h"
+
+/**
+ * cap_string - The description is below
+ * Description - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int m, k;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (m = 0; s[m] != '\0'; m++)
+	{
+		if (m == 0 && s[m] >= 'a' && s[m] <= 'z')
+			s[m] -= 32;
+
+		for (k = 0; k < 13; k++)
+		{
+			if (s[m] == spe[k])
+			{
+				if (s[m + 1] >= 'a' && s[m + 1] <= 'z')
+				{
+					s[m + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
