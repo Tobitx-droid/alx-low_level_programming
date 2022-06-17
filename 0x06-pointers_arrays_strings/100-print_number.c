638f162deaa8bc7e97cf47e5afffc2ef4f4e1db0
Tobitx-droid <67651574+Tobitx-droid@users.noreply.github.com>
2022-06-17 01:32:35 +0000

    Create 100-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - The description is below
+ * Description: prints an integer;
+ * @n: integer to be printed;
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	      unsigned int k;
+
+	      if (n < 0)
+      	{
+	            	k = -n;
+            		_putchar('-');
+      	} else
+	      {
+	            	k = n;
+      	}
+
+      	if (k / 10)
+           		 print_number(k / 10);
+
+	      _putchar((k % 10) + '0');
+}
