aebe2c471758b7265f126e839bf71307cd41c461
Tobitx-droid <tobitamofaadubredu@gmail.com>
2022-06-17 11:44:06 -0700

    Pointers practice

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,19 +8,20 @@
  */
 void print_number(int n)
 {
-	      unsigned int k;
 
-	      if (n < 0)
+        unsigned int k;
+
+        if (n < 0)
       	{
-	            	k = -n;
-            		_putchar('-');
+	       	k = -n;
+            	_putchar('-');
       	} else
-	      {
-	            	k = n;
+        {
+	        k = n;
       	}
 
       	if (k / 10)
-           		 print_number(k / 10);
+                print_number(k / 10);
 
-	      _putchar((k % 10) + '0');
+        _putchar((k % 10) + '0');
 }
